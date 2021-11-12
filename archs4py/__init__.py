d42cd56375b7d2eb4ffac939ea83dc4842c480ac
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2021-11-12 13:56:34 -0500

    add postprocessing

--- archs4py/__init__.py
+++ archs4py/__init__.py
@@ -11,6 +11,7 @@ import re
 from archs4py import db
 from archs4py import sra
 from archs4py import geo
+from archs4py import postprocessing
 
 import importlib
 importlib.reload(db)
@@ -20,6 +21,7 @@ def reload():
     importlib.reload(db)
     importlib.reload(sra)
     importlib.reload(geo)
+    importlib.reload(postprocessing)
 
 def test():
     print("cool")
